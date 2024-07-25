a2855e118eb14996d8019ce5c824d6f8aaf16367
octavioromagnoli <octaromag@gmail.com>
2024-07-25 03:48:07 -0300

    Add functionality, flexibility and fixed bugs
    Added the main functionality through event listeners that catch
    the mouse over the main-container. This is so that I can catch the
    target and change its background color. Also added so that by calling
    the function createSquares you can define the amount and sizes through
    parameters.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
-function createSquares(amount, height, borderPx) {
-  const mainContainer = document.querySelector(".main-container");
-  mainContainer.style.cssText = `width: ${(height + borderPx*2) * amount}px; margin: 20px auto;`;
+function createSquares(mainContainer, amount, height, borderPx) {
+  mainContainer.innerHTML = "";
+  mainContainer.style.cssText = `width: ${(height + borderPx*2) * amount}px;`;
   for (let i = 0; i < amount; i++) {
     for (let j = 0; j < amount; j++) {
       let squareDiv = document.createElement("div");
       squareDiv.setAttribute("class", "squareDiv");
+      squareDiv.style.cssText = `height: ${height}px; width: ${height}px; border: ${borderPx}px solid;`;
       mainContainer.appendChild(squareDiv);
     }
-    let skipLine = document.createElement("br");
-    mainContainer.appendChild(skipLine);
   }
 }
 
-createSquares(10, 30, 1);
+const mainContainer = document.querySelector(".main-container");
+createSquares(mainContainer, 14, 30, 3);
+
+mainContainer.addEventListener("mousemove", (event) =>{
+  let target = event.target;
+  if(target.className === "squareDiv"){
+    target.classList.add("selected");
+  }
+})
+
